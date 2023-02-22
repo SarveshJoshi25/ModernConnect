a1ccaab7acc9860bd6a82acef6ca00d1734dad1e
SarveshJoshi25 <contact.sarveshjoshi@gmail.com>
2023-02-22 23:28:27 +0530

    Added CORS settings

--- ModernConnect2/settings.py
+++ ModernConnect2/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     "modernConnect",
+    'corsheaders',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -150,7 +152,7 @@ AUTH_USER_MODEL = 'modernConnect.UserAccount'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-
+CORS_ORIGIN_ALLOW_ALL = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
